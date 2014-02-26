325ab6786c36ecd6067508d5b29bb044f9ff6d0a
Philip <philip.burenstam.linder@gmail.com>
2014-02-26 17:29:52 +0100

    Påbörjat journalManager

--- Server/journalManager.java
+++ Server/journalManager.java
@@ -1,4 +1,8 @@
 	public class journalManager{
+		private static final int PATIENT = 0;
+		private static final int NURSE = 1;
+		private static final int DOCTOR = 2;
+		private static final int GOVERNMENT = 3;
 		
 		//klassen som innehåller alla Journaler
 
@@ -9,11 +13,34 @@
 		//Lägg till en person i registret av journaler genom att lägga in dens personnummer och journal i mappen
 		journals.put(idNbr, journal);
 	}
-	public boolean deletePatient(int idNbr){
-		return journals.remove(idNbr);
+	public boolean deletePatient(String idNbr, int type){
+			deletePatient(idNbr);
+			return journals.remove(idNbr);
 	//Tar bort en person ur journalregistret med angivet personnummer	
 	}
-	public journal getJournal(int idNbr){
+	public journal readJournal(String idNbr, int type){
+		if(type == PATIENT || type GOVERNMENT){
+			return journals.get(idNbr);
+		} else if(type == NURSE) {
+			Nurse nurse = nurseMap.get(staffID);
+			if(nurse.getDivision().equals("Division of patient") || nurse.getPatients().equals("Nurse of patient")){
+				return journals.get(idNbr);
+			} else {
+				Doctor doctor = doctorMap.get(staffID);
+				if(doctor.getDivision().equals("Division of patient") || doctor.getPatients().equals("Doctor of patient"))){
+					return journals.get(idNbr);
+				} else {
+					return false;
+				}
+
+
+	}
+				return false;
+			}
+
+		} else{				//Type is Doctor
+
+		}
 		//returnerar hela journalen för en viss person
 		journals.get(idNbra);
 	}
