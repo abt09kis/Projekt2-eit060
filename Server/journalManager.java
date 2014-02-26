cc8ada5120c2235c3393ba7a62e120f3a4a5b2e5
Philip <philip.burenstam.linder@gmail.com>
2014-02-26 18:01:26 +0100

    sdd

--- Server/journalManager.java
+++ Server/journalManager.java
@@ -15,38 +15,26 @@
 	}
 
 	//Tar bort en person ur journalregistret med angivet personnummer
-	public boolean deletePatient(String idNbr, int type){
+	public boolean deletePatient(String idNbr){
 			deletePatient(idNbr);
 			return journals.remove(idNbr);
 		}
 
 	//Tar bort en person ur journalregistret med angivet personnummer	
 	}
-	public journal readJournal(String idNbr, int type){
+	public journal readJournal(String idNbr, String staffID, int type){
 		if(type == PATIENT || type GOVERNMENT){
 			return journals.get(idNbr);
 		} else if(type == NURSE) {
 			Nurse nurse = nurseMap.get(staffID);
-			if(nurse.getDivision().equals("Division of patient") || nurse.getPatients().equals("Nurse of patient")){
+			if(nurse.getDivision().equals("Division of pati") || nurse.getPatients().equals("Nurse of patient")){
 				return journals.get(idNbr);
 			} else {
 				Doctor doctor = doctorMap.get(staffID);
 				if(doctor.getDivision().equals("Division of patient") || doctor.getPatients().equals("Doctor of patient"))){
 					return journals.get(idNbr);
-				} else {
-					return false;
 				}
-
-
-	}
-				return false;
 			}
-
-		} else{				//Type is Doctor
-
-		}
-		//returnerar hela journalen för en viss person
-		journals.get(idNbra);
 	}
 	public boolean writeTo(int idNbr, String text, String nurseiId, String docId, String div, String date){
 
