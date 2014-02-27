762d10227e712c2527d3d3438b4ce442f4952b62
Kevin Ispiroglu Linblad <abt09kis@student.lu.se>
2014-02-27 11:43:46 +0100

    la upp doctor

---
+++ Server/Doctor.java
@@ -0,0 +1,28 @@
+package server;
+
+public class Doctor{
+
+	private boolean isDoctor;
+	private String unit;
+	private String id:
+	private String name;
+
+	public Doctor(String unit, String id, String name, boolean isDoctor){
+		this.unit=unit;
+		this.id=id;
+		this.name=name;
+		this.isDoctor=isDoctor;
+
+	}
+	public boolean isDoctor(){
+		return isDoctor;
+	}
+
+	public String getId(){
+		return id;
+	}
+
+	public String getUnit(){
+		return unit;
+	}
+}
