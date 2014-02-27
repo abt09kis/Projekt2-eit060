329a4e45e74a0cd6ab843b21fba49b80e0ecb4b7
soderb <dat12eso@student.lu.se>
2014-02-27 14:06:37 +0100

    tje

--- Client/Main.java
+++ Client/Main.java
@@ -1,4 +1,4 @@
-	
+
 
 	boolean close = false;
 	scanner = new Scanner(System.in);
@@ -37,7 +37,6 @@
 		}
 
 		} else if (id.equals(nurse) {
-				while(!close){
 				System.out.println("Welcome nurse, what would you like to do?");
 				System.out.println("1. Read a journal");
 				System.out.println("2. Write to a journal");
@@ -48,14 +47,11 @@
 
 				nurseMenu(choice, id);
 
-				}else{
-					close=true;
 				}
 				}
 			}
 		} else {
 			System.out.println("Welcome goverment, what would you like to do?");
-			while(!close){
 				System.out.println("1. Read a journal");
 				System.out.println("2. Delete a journal");
 				System.out.println("4. exit");
@@ -64,16 +60,16 @@
 
 				if(choice!=4){
 				govMenu(choice, id);	
-				]else{
-					close=true;
 				}
 			}
 		}
 
 		private void doctor(id, choice){
+				
 
 			//case-satser utifrån vad läkaren har valt att han vill göra!!
 				switch(choice){
+			
 
 					case 1: //Läs en journalEntry
 						Sysytem.out.Println("Enter PatientID:")
@@ -108,11 +104,9 @@
 
 						public void addJournal(id, newJournal, newPatient);
 
-					case 4: //stäng
-
-
 
 				}
+			}
 
 		}
 		private void patient(idNbr){
@@ -122,20 +116,58 @@
 				temp.getJournal.printJournal;
 
 		}
-		private void government(name, choice)
+		private void government(name, choice){
 
 		switch(choice){
-
 			case 1:
 			readJournal
 			
 						Sysytem.out.Println("Enter PatientID:")
 						
 					String patId = scanner.next();
-						readJournal(patId, id, 2);
+						readJournal(patId, id, 3);
 
 			case 2:
-			deleteJournal
-			exit
+
+			Sysytem.out.Println("Enter PatientID for removal:")
+			String patId = scanner.next();
+
+			public boolean deletePatient(String idNbr);
+
+			
+
+
 		}
-		private void nurse (id, choice)
+	}
+	}
+		private void nurse (id, choice){
+
+					case 1: //Läs en journalEntry
+					
+					Sysytem.out.Println("Enter PatientID:")
+						
+					String patId = scanner.next();
+						readJournal(patId, id, 1);
+
+					case 2:	//Lägg till text i en existerande journal entry
+					
+						System.out.Println("Enter nurseId");
+						String nurseId = scanner.next();
+						System.out.Println("Enter docId");
+						String docId scanner.next();
+						System.out.Println("Enter div");
+						String div = scanner.next();
+						System.out.Println("Enter date");
+						String date = scanner.next();
+						System.out.Println("Enter text");
+						String text = scanner.next();
+
+						writeTo(id, text, nurseId, docId, div, date);
+					
+		}
+
+
+
+
+
+
